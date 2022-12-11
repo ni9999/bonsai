defee9d4be17085954aac6272e8122e99a16de22
ni9999 <nazmulislamzrn@yahoo.com>
2022-12-11 11:02:41 +0000

    Added codes

--- bonsai/text/views.py
+++ bonsai/text/views.py
@@ -56,19 +56,38 @@ def output(request):
     
     prompt = request.session.get('text')
 
+    sum = f"""summarize shortly these passage. If you want to finish use '--'. {additionalprompt}:
+    
+Passage: Is Wordle getting tougher to solve? Players seem to be convinced that the game has gotten harder in recent weeks ever since The New York Times bought it from developer Josh Wardle in late January. The Times has come forward and shared that this likely isn't the case. That said, the NYT did mess with the back end code a bit, removing some offensive and sexual language, as well as some obscure words There is a viral thread claiming that a confirmation bias was at play. One Twitter user went so far as to claim the game has gone to "the dusty section of the dictionary" to find its latest words.
+
+TLDR: Wordle has not gotten more difficult to solve.
+--
+Passage: ArtificialIvan, a seven-year-old, London-based payment and expense management software company, has raised $190 million in Series C funding led by ARG Global, with participation from D9 Capital Group and Boulder Capital. Earlier backers also joined the round, including Hilton Group, Roxanne Capital, Paved Roads Ventures, Brook Partners, and Plato Capital.
+
+TLDR: ArtificialIvan has raised $190 million in Series C funding.
+--
+Passage: {prompt}
+
+TLDR:"""
 
     if 'summary' in request.POST:
-        prompt = "get the summary of this article." + additionalprompt +"\n"+prompt
+        prompt = sum
+        response = co.generate( 
+            model='xlarge', 
+            prompt = prompt,
+            max_tokens=40, 
+            temperature=0.8,
+            stop_sequences=["--"])
+    
     else:
-        prompt = "complete the following text." + additionalprompt +"\n"+prompt
+        prompt = "complete the following story." + additionalprompt +". \nstory:"+prompt
+        response = co.generate(  
+            model='xlarge',  
+            prompt = prompt,  
+            max_tokens=40,  
+            temperature=0.6,  
+            stop_sequences=["--"])
 
-
-    response = co.generate( 
-        model='medium', 
-        prompt = prompt,
-        max_tokens=400, 
-        temperature=0.8,
-        stop_sequences=["--"])
 
     out = response.generations[0].text
     context = {
